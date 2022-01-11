299e82e0dfc01734d5d4ba722ead6c6f739ae422
Zach <zmommaerts@gmail.com>
2022-01-11 11:15:46 -0600

    stuck

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,40 @@
+class SpicesController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_response
+
+    def index
+        render json: Spice.all, except: [:created_at, :updated_at]
+    end
+
+    def show
+        render json: find_spice, except: [:created_at, :updated_at]
+    end
+
+    def create
+        spice = Spice.create(spice_params)
+        render json: spice, except: [:created_at, :updated_at], status: :created
+    end
+
+    def update
+        spice = Spice.update(spice_params)
+        render json: spice
+    end
+
+    def destroy
+        find_spice.destroy
+        head :no_content
+    end
+
+    private
+
+    def record_not_found_response
+        render json: { error: 'Spice not found' }, status: :not_found
+    end
+
+    def find_spice
+        Spice.find(params[:id])
+    end
+
+    def spice_params
+        params.permit(:title, :image, :description, :notes, :rating)
+    end
+end
